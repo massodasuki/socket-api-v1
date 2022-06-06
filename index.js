d9bba44637d2fbf176792120017d4f8a8922c006
massodasuki <massolihen.dasuki@razer.com>
2022-06-07 03:44:53 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,123 @@
+require('dotenv').config();
+const app = require('express')()
+const http = require('http').createServer(app)
+const uuid4 = require('uuid4')
+const FormData = require('form-data');
+const socketio = require('socket.io')(http)
+const axios = require('axios').default;
+
+
+var Storage = require('node-storage');
+var store = new Storage('path/to/file');
+
+
+// process.env.USER_ID; // "239482"
+// process.env.USER_KEY; // "foobar"
+// process.env.NODE_ENV; // "development"
+
+
+app.get('/', (req, res) => {
+    res.send("Node Server is running. Yay!!")
+})
+
+
+socketio.on('connect', socket => {
+    var roomName, people;
+    socket.on('join', (room) => {
+    console.log(room);
+    var roomA = ''+room.from +'-'+ room.to+'';
+    var roomB = ''+room.to +'-'+ room.from+'';
+
+    // store.put(roomA, 'world');
+    var roomName = store.get(roomA) ? store.get(roomA) : store.get(roomB);
+    if (roomName) {
+      console.log("Join Room");
+      socket.join(roomName);
+    }
+    else {
+      console.log("Create Room");
+      var roomUuid = uuid4();
+      store.put(roomA, roomUuid);
+      store.put(roomB, roomUuid);
+      roomName = roomUuid;
+      socket.join(roomName);
+    }
+
+    const form = new FormData();
+    form.append('my_id', room.from);
+    form.append('to_id', room.to);
+    form.append('offset', 0);
+
+    axios({
+      method  : 'post',
+      url     : 'https://hafiz.work/api/mobile/open-chat',
+      headers : form.getHeaders(),
+      data    : form
+    })
+    .then((resolve) => {
+      conversation = resolve.data;
+      // console.log(conversation);
+      conversation.room = roomName;
+      conversation.people = people;
+
+      // console.log(roomName);
+      socketio.to(roomName).emit('room', conversation);      
+    })
+    .catch((error) => console.log(error));
+
+
+  });
+
+    socket.on('send_message', (newMessage, callback) => {
+        var roomName = newMessage.room;
+
+        const form = new FormData();
+        // console.log(newMessage);
+        form.append('from', newMessage.from);
+        form.append('to', newMessage.to);
+        form.append('msg', newMessage.msg);
+        form.append('media1',"");
+        form.append('media_type',"");
+
+        axios({
+          method  : 'post',
+          url     : 'https://hafiz.work/api/mobile/submit-chat',
+          headers : form.getHeaders(),
+          data    : form
+        })
+        .then((resolve) => {
+          console.log(resolve.data);
+
+          const form = new FormData();
+          form.append('my_id', newMessage.from);
+          form.append('to_id', newMessage.to);
+          form.append('offset', 0);
+
+          axios({
+            method  : 'post',
+            url     : 'https://hafiz.work/api/mobile/open-chat',
+            headers : form.getHeaders(),
+            data    : form
+          })
+          .then((resolve) => {
+            conversation = resolve.data;
+            // console.log(conversation);
+            conversation.room = roomName;
+            conversation.people = people;
+
+            // console.log(roomName);
+            socketio.to(roomName).emit('room', conversation);      
+          })
+          .catch((error) => console.log(error));
+        })
+        .catch((error) => console.log(error));
+              
+      });
+    
+});
+
+
+// http.listen(process.env.PORT)
+http.listen(3000)
+
+

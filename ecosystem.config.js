522551e4128aea1a5575254bf19ecad3e5dce30d
massodasuki <massodasuki@gmail.com>
2023-10-16 20:33:44 +0800

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,10 @@
 module.exports = {
   apps : [{
     script: 'main.js',
-    instances: 'max', // 'max' will create as many instances as available CPU cores
+    instances: '8', // 'max' will create as many instances as available CPU cores
     autorestart: true,
     cron_restart: '0 21 * * *', // Cron schedule 5 am Malaysia time
     exec_mode: 'cluster',
-    watch: '.',
-    args: '--max-old-space-size=2048'
+    watch: '.'
   }]
 };
